8b6dba3840013fdc2fcc2a620f35ba8d6149c779
T. Kunze <kunze@wangaz.com>
2018-08-31 18:14:15 +0200

    update seeded table

--- database/seeds/ExampleItemSeeder.php
+++ database/seeds/ExampleItemSeeder.php
@@ -18,6 +18,14 @@ class ExampleItemSeeder extends Seeder
         $test->type = "project";
         $test->active = true;
         $test->name = "Example Test";	// Name your test
+        
+        $test->repetitions = 0;
+		$test->repetitions_interval = 0;
+		$test->time_limit = 5000;
+		$test->continueable = false;
+		$test->reporting = true;
+		$test->caching = true;
+        
         $test->save();
 
         // create items
