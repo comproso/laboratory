8650d98d0d347b798b262cbe963812f52ea6415e
T. Kunze <kunze@wangaz.com>
2018-09-14 19:09:27 +0200

    move Semantic UI

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,9 +3,9 @@
     <head>
         <title>comproso - laboratory</title>
         <meta charset="UTF-8">
-        <link href="{{ asset('assets/semantic-ui/semantic.min.css') }}" type="text/css" rel="stylesheet">
-		<script src="{{ asset('assets/jquery/jquery.min.js') }}" type="text/javascript"></script>
-		<script src="{{ asset('assets/semantic-ui/semantic.min.js') }}" type="text/javascript"></script>
+        <link href="vendor/semantic-ui/semantic.min.css" type="text/css" rel="stylesheet">
+		<script src="vendor/jquery/jquery.min.js" type="text/javascript"></script>
+		<script src="vendor/semantic-ui/semantic.min.js" type="text/javascript"></script>
 		@yield('head')
     </head>
     <body class="@yield('bodycss')">
